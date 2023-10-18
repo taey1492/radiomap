669f2cf17b220aee62ec424714790041d5081807
taeyoon <1492k@naver.com>
2023-10-18 17:10:08 +0900

    first

---
+++ src/main/java/com/example/radio/RadioApplication.java
@@ -0,0 +1,13 @@
+package com.example.radio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioApplication.class, args);
+	}
+
+}
