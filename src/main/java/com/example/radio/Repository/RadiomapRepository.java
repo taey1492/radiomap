b8647a47e5c4a71d486400ef34f013da2782005e
taeyoon <1492k@naver.com>
2023-10-19 12:44:27 +0900

    as

--- src/main/java/com/example/radio/Repository/RadiomapRepository.java
+++ src/main/java/com/example/radio/Repository/RadiomapRepository.java
@@ -1,6 +1,5 @@
 package com.example.radio.Repository;
 
-
 import com.example.radio.Model.Radiomap;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
