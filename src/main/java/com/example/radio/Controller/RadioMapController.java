5815df9290303cee8285b456c8750c126b8ea726
cacoke6 <cacoke6@gmail.com>
2023-10-20 10:23:43 +0900

    수정완료0

--- src/main/java/com/example/radio/Controller/RadioMapController.java
+++ src/main/java/com/example/radio/Controller/RadioMapController.java
@@ -1,10 +1,14 @@
 package com.example.radio.Controller;
 
+import com.example.radio.Model.Radiodata;
 import com.example.radio.Repository.RadioDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/map")
--- src/main/java/com/example/radio/Model/Radiodata.java
+++ src/main/java/com/example/radio/Model/Radiodata.java
@@ -2,16 +2,44 @@ package com.example.radio.Model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class Radiodata {
-    @Id
-    int id;
     int year;
     int month;
     String item;
+    @Id
     String pinnacle;
+    String coldate;
+    String depth;
+
+//    @Column(name = "134Cs(mBq/kg)")
+    float cs134;
+
+//    @Column(name = "137Cs(mBq/kg)")
+    float cs137;
+
+//    @Column(name = "3H(Bq/L)")
+    float H3;
+
+//    @Column(name = "zenbeta(Bq/L)")
+    float zenbeta;
+
+//    @Column(name = "90Sr(mBq/kg)")
+    float Sr90;
+
+//    @Column(name = "239+240Pu(μBq/kg)")
+    float Pu239;
+
+//    @Column(name = "240Pu/239Pu")
+    float Pu240;
+
+
+
+
+
 }
