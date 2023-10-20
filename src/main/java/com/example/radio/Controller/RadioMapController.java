fdafa33d01ccfbe0ef99330cfb5c96adcb859440
minchonang <green8085@naver.com>
2023-10-20 16:43:05 +0900

    경고 코드 삭제

--- src/main/java/com/example/radio/Controller/RadioMapController.java
+++ src/main/java/com/example/radio/Controller/RadioMapController.java
@@ -1,16 +1,10 @@
 package com.example.radio.Controller;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
-import com.example.radio.Model.Radiodata;
 import com.example.radio.Repository.RadioDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/map")
