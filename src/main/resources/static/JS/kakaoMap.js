76d1cb5f8090527b1507d9b9d125d4ffb1b05c0d
minchonang <green8085@naver.com>
2023-10-23 15:17:46 +0900

    마커 팝업(임시)

--- src/main/resources/static/JS/kakaoMap.js
+++ src/main/resources/static/JS/kakaoMap.js
@@ -28,7 +28,7 @@ map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 // 마커 표시하기
 
 // 마커를 표시할 위치와 title
-const radioNormalPoint = [
+const radioPoint = [
 	{
 		title: "카카오",
 		latlng: new kakao.maps.LatLng(35.450705, 127.070677),
@@ -75,12 +75,12 @@ const radioRedPoint = [
 
 // 마커 이미지의 이미지 주소입니다
 const iconRadioNormal = "../img/icon_radioactivity_mono.png";
-const iconRadioYellow = "../img/icon_radioactivity_red.png";
-const iconRadioRed = "../img/icon_radioactivity_yellow.png";
+const iconRadioYellow = "../img/icon_radioactivity_yellow.png";
+const iconRadioRed = "../img/icon_radioactivity_red.png";
 
 // 방사능 - 정상
 
-for (let i = 0; i < radioNormalPoint.length; i++) {
+for (let i = 0; i < radioPoint.length; i++) {
 	// 마커 이미지의 이미지 크기 입니다
 	const imageSize = new kakao.maps.Size(30, 30);
 
@@ -90,110 +90,47 @@ for (let i = 0; i < radioNormalPoint.length; i++) {
 	// 마커를 생성합니다
 	const marker = new kakao.maps.Marker({
 		map: map, // 마커를 표시할 지도
-		title: radioNormalPoint[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-		position: radioNormalPoint[i].latlng, // 마커를 표시할 위치
+		title: radioPoint[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+		position: radioPoint[i].latlng, // 마커를 표시할 위치
 		image: markerImage, // 마커 이미지
 		clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
 	});
-	// 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-	const iwContent = '<div style="padding:10px;">정상입니다.</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-		iwRemoveable = true; // removeable 속성을 true 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
-	// 인포윈도우를 생성합니다
-	const infowindow = new kakao.maps.InfoWindow({
-		content: iwContent,
-		removable: iwRemoveable,
+	// 커스텀 오버레이에 표시할 컨텐츠 입니다
+	// 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
+	// 별도의 이벤트 메소드를 제공하지 않습니다
+	const content =
+		'<div class="wrap">' +
+		'    <div class="info">' +
+		'        <div class="title">' +
+		`            ${radioPoint[i].title}` +
+		'            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
+		"        </div>" +
+		'        <div class="body">' +
+		'            <div class="desc">' +
+		'                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+		'                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+		'                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+		"            </div>" +
+		"        </div>" +
+		"    </div>" +
+		"</div>";
+
+	// 마커 위에 커스텀오버레이를 표시합니다
+	// 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+	const overlay = new kakao.maps.CustomOverlay({
+		content: content,
+		map: map,
+		position: marker.getPosition(),
 	});
 
-	// 마커에 마우스오버 이벤트를 등록합니다
-	kakao.maps.event.addListener(marker, "mouseover", function () {
-		// 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-		infowindow.open(map, marker);
+	// 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+	kakao.maps.event.addListener(marker, "click", function (event) {
+		overlay.setMap(map);
 	});
 
-	// 마커에 마우스아웃 이벤트를 등록합니다
-	kakao.maps.event.addListener(marker, "mouseout", function () {
-		// 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-		infowindow.close();
-	});
-}
-
-// 방사능 - 경고
-
-for (let i = 0; i < radioYellowPoint.length; i++) {
-	// 마커 이미지의 이미지 크기 입니다
-	const imageSize = new kakao.maps.Size(30, 30);
-
-	// 마커 이미지를 생성합니다
-	const markerImage = new kakao.maps.MarkerImage(iconRadioYellow, imageSize);
-
-	// 마커를 생성합니다
-	const marker = new kakao.maps.Marker({
-		map: map, // 마커를 표시할 지도
-		title: radioYellowPoint[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-		position: radioYellowPoint[i].latlng, // 마커를 표시할 위치
-		image: markerImage, // 마커 이미지
-		clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-	});
-	// 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-	const iwContent = '<div style="padding:10px;">위험합니다.</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-		iwRemoveable = true; // removeable 속성을 true 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-	// 인포윈도우를 생성합니다
-	const infowindow = new kakao.maps.InfoWindow({
-		content: iwContent,
-		removable: iwRemoveable,
-	});
-
-	// 마커에 마우스오버 이벤트를 등록합니다
-	kakao.maps.event.addListener(marker, "mouseover", function () {
-		// 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-		infowindow.open(map, marker);
-	});
-
-	// 마커에 마우스아웃 이벤트를 등록합니다
-	kakao.maps.event.addListener(marker, "mouseout", function () {
-		// 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-		infowindow.close();
-	});
-}
-
-// 방사능 - 위험
-
-for (let i = 0; i < radioRedPoint.length; i++) {
-	// 마커 이미지의 이미지 크기 입니다
-	const imageSize = new kakao.maps.Size(30, 30);
-
-	// 마커 이미지를 생성합니다
-	const markerImage = new kakao.maps.MarkerImage(iconRadioRed, imageSize);
-
-	// 마커를 생성합니다
-	const marker = new kakao.maps.Marker({
-		map: map, // 마커를 표시할 지도
-		title: radioRedPoint[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-		position: radioRedPoint[i].latlng, // 마커를 표시할 위치
-		image: markerImage, // 마커 이미지
-		clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-	});
-	// 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-	const iwContent = '<div style="padding: 10px;">경고합니다.</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-		iwRemoveable = true; // removeable 속성을 true 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-	// 인포윈도우를 생성합니다
-	const infowindow = new kakao.maps.InfoWindow({
-		content: iwContent,
-		removable: iwRemoveable,
-	});
-
-	// 마커에 마우스오버 이벤트를 등록합니다
-	kakao.maps.event.addListener(marker, "mouseover", function () {
-		// 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-		infowindow.open(map, marker);
-	});
-
-	// 마커에 마우스아웃 이벤트를 등록합니다
-	kakao.maps.event.addListener(marker, "mouseout", function () {
-		// 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-		infowindow.close();
-	});
+	// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
+	function closeOverlay() {
+		overlay.setMap(null);
+	}
 }
