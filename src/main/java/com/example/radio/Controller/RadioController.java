30ca545f4cdef7b6735c4c56faf0473fc6bbf6df
cacoke6 <cacoke6@gmail.com>
2023-10-23 16:58:07 +0900

    etity수정,모델수정, 급해서수정

--- src/main/java/com/example/radio/Controller/RadioController.java
+++ src/main/java/com/example/radio/Controller/RadioController.java
@@ -1,35 +1,61 @@
 package com.example.radio.Controller;
 
 
+import com.example.radio.Model.Earthquake;
 import com.example.radio.Model.RadioData;
+import com.example.radio.Model.RadioMap;
 import com.example.radio.Repository.RadioDataRepository;
+import com.example.radio.Repository.RadioMapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/map")
 public class RadioController {
 
+
     @Autowired
-    RadioDataRepository radioDataRepository;
+    RadioMapRepository radioMapRepository;
 
-    @GetMapping("/index")
 
-    public String index() {
+    @GetMapping("testy")
+    public  String index(@ModelAttribute RadioMap radioMap,Model model) {
 
-        return "/map/index";
+
+
+
+        return "/map/testy";
+
     }
 
-     @PostMapping("/index")
-     public String indexPost(@ModelAttribute RadioData radiodata) {
+
+     @PostMapping("/testy")
+     public String indexPost(@ModelAttribute RadioMap radioMap ,Model model) {
+
+        int i = radioMap.getId();
+
+        Optional<RadioMap> latlon = radioMapRepository.findById(i);
+
+        Float lat = latlon.get().getLat();
+        Float lon = latlon.get().getLon();
+
+        List<Float> l = new ArrayList<>();
+        l.add(lat);
+        l.add(lon);
 
 
+        model.addAttribute("latlonlist", l);
 
-     return "redirect:/mpa/index";
+        System.out.println(l);
+
+
+         return "redirect:/map/testy";
      }
 
 }
