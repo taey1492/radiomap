de19a4dadde5ae58ccef01c71ed4f87cf312c2ed
minchonang <green8085@naver.com>
2023-10-24 16:39:53 +0900

    kakaoMap.js 주석 수정

--- src/main/java/com/example/radio/Model/Earthquake.java
+++ src/main/java/com/example/radio/Model/Earthquake.java
@@ -1,5 +1,6 @@
 package com.example.radio.Model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,22 +13,36 @@ public class Earthquake {
 
   @Id
   @GeneratedValue
+
+  @Column(name = "ID")
   int id;
-
+  @Column(name = "NUM")
   int num;
-
+  @Column(name = "EARYEAR")
   int eqyear;
+  @Column(name = "EARMONTH")
   int eqmonth;
+  @Column(name = "EARDAY")
   int eqday;
+  @Column(name = "EQRTIME")
   String eqtime;
+  @Column(name = "ANTEMERI")
   String antemeri;
+  @Column(name = "EARSCALE")
   Float eqscale;
+  @Column(name = "DEEP")
   String deep;
+  @Column(name = "MAGNITUDE")
   String magnitude;
+  @Column(name = "LAT")
   Float lat;
+  @Column(name = "LON")
   Float lon;
+  @Column(name = "LOCATION")
   String location;
+  @Column(name = "MAPLOC")
   String maploc;
+  @Column(name = "DETAIL")
   String detail;
 
 }
