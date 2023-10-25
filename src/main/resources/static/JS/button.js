97f8b014e51420d60d0c31e3e34db18558465b55
minchonang <green8085@naver.com>
2023-10-25 12:17:35 +0900

    방사능, 지진 버튼 누를 때 마커 설명 바뀌는 기능 추가

--- src/main/resources/static/JS/button.js
+++ src/main/resources/static/JS/button.js
@@ -1,15 +1,22 @@
 const MENUBARBTN = "menuBarBtn";
 const SELECTEDBTN = "selectedBtn";
 
-let boolRadioBtn = true;
-let boolEQBtn = false;
+const RADIOMARKERINFOAREA = "radioMarkerInfoArea";
+const EQMARKERINFOAREA = "eqMarkerInfoArea";
 
 const radio = document.querySelector("#radioBtn");
 const eq = document.querySelector("#eqBtn");
+const radioMarker = document.querySelector("#radioMarkerInfo");
+const eqMarker = document.querySelector("#eqMarkerInfo");
+
+let boolRadioBtn = true;
+let boolEQBtn = false;
 
 const selectRadioBtn = (event) => {
 	if (!boolRadioBtn) {
 		radio.classList.replace(MENUBARBTN, SELECTEDBTN);
+		radioMarker.classList.replace("hidden", RADIOMARKERINFOAREA);
+		eqMarker.classList.replace(EQMARKERINFOAREA, "hidden");
 		eq.classList = MENUBARBTN;
 		boolRadioBtn = true;
 		boolEQBtn = false;
@@ -18,6 +25,8 @@ const selectRadioBtn = (event) => {
 const selectEQBtn = (event) => {
 	if (!boolEQBtn) {
 		eq.classList.replace(MENUBARBTN, SELECTEDBTN);
+		eqMarker.classList.replace("hidden", EQMARKERINFOAREA);
+		radioMarker.classList.replace(RADIOMARKERINFOAREA, "hidden");
 		radio.classList = MENUBARBTN;
 		boolRadioBtn = false;
 		boolEQBtn = true;
