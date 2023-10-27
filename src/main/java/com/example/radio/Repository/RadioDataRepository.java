d289c248bac77177bde545fd4ff05f366996a800
taeyoon <1492k@naver.com>
2023-10-27 16:08:49 +0900

    radiodata.js radiosearch2 url변경

--- src/main/java/com/example/radio/Repository/RadioDataRepository.java
+++ src/main/java/com/example/radio/Repository/RadioDataRepository.java
@@ -3,8 +3,10 @@ package com.example.radio.Repository;
 import com.example.radio.Model.RadioData;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +14,12 @@ public interface RadioDataRepository extends JpaRepository<RadioData, Integer> {
 
      List<RadioData> findByYear(int year);
 
+     // @Query(nativeQuery = true, value = "select * " + //
+     // " from radio_data rd " + //
+     // " left join radio_address ra " + //
+     // " on rd.r_pinnacle = ra.r_pinnacle" + //
+     // " left join radio_map rm " + //
+     // " on ra.r_code = rm.r_code " + //
+     // " where rd.r_year = :1")
+     // List<Map<String, Object>> findByYear2(int year);
 }
