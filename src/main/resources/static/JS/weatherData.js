6d1e34ec543b52088cd5858edbd85aff06d0ffdb
minchonang <green8085@naver.com>
2023-10-30 14:35:51 +0900

    주석 정리

--- src/main/resources/static/JS/weatherData.js
+++ src/main/resources/static/JS/weatherData.js
@@ -6,12 +6,13 @@ const weatherMarkers = [];
 
 // 날씨 api json으로 불러오기
 const weatherDataPush = async () => {
+	// 브라우저의 현재 시간 가져오기
 	let now = new Date();
 	let year = now.getFullYear();
 	let month = now.getMonth() + 1;
 	let date = now.getDate();
 	let hours = now.getHours() - 1;
-	//0시, 1시 앞에 0 붙이기
+	// 0시, 1시 앞에 0 붙이기
 	hours = String(hours).padStart(2, "0");
 	let min = now.getMinutes();
 	if (min >= 55) min = "30";
@@ -66,7 +67,6 @@ const weatherDataPush = async () => {
 		let weatherCity = weatherLocation[i].city; // 지역
 
 		// 지역에 따른 마커 배정
-		// marker_weather = "../img/marker_weather_Seoul.png";
 		if (weatherCity == "서울")
 			marker_weather = "../img/marker_weather_서울.png";
 		else if (weatherCity == "경기")
@@ -100,6 +100,7 @@ const weatherDataPush = async () => {
 		else if (weatherCity == "강원")
 			marker_weather = "../img/marker_weather_강원.png";
 
+		// 풍향 각도에 따라 분류
 		if (weatherWind >= 315) weatherWind = "북서북";
 		else if (weatherWind >= 270) weatherWind = "서북서";
 		else if (weatherWind >= 225) weatherWind = "남서서";
@@ -199,58 +200,3 @@ const weatherClose = () => {
 		weatherMarkers[i].setMap(null);
 	}
 };
-// ------------------------------------
-
-// $.ajax({
-// 	url: "/weather/weather", // JSON 데이터를 받아올 URL.
-// 	type: "GET",
-// 	success: function (data) {
-// 		const weatherJson = data.map(function (weather) {
-// 			return {
-// 				addr: weather.addr,
-// 				obs_nm: weather.obs_nm,
-// 				latlng: new kakao.maps.LatLng(weather.lat, weather.lon),
-// 			};
-// 		});
-
-// 		for (let i = 0; i < weatherJson.length; i++) {
-// 			// 마커 이미지의 이미지 크기 입니다
-// 			const imageSize = new kakao.maps.Size(24, 35);
-
-// 			// 마커 이미지를 생성합니다
-// 			const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-// 			// 마커를 생성합니다
-// 			const marker = new kakao.maps.Marker({
-// 				map: map, // 마커를 표시할 지도
-// 				position: weatherJson[i].latlng, // 마커를 표시할 위치
-// 				title: weatherJson[i].addr, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-// 				sub: weatherJson[i].obs_nm,
-// 				image: markerImage, // 마커 이미지
-// 			});
-
-// 			marker.setSub = function (depth) {
-// 				// 마커 객체에 depth를 설정하는 함수를 추가합니다.
-// 				this.obs_nm = depth;
-// 			};
-
-// 			marker.getSub = function () {
-// 				// 마커 객체에서 depth를 가져오는 함수를 추가합니다.
-// 				return this.obs_nm;
-// 			};
-
-// 			marker.setSub(weatherJson[i].obs_nm); // depth를 설정합니다.
-
-// 			addMarkerClickListener(marker);
-// 		}
-
-// 		function addMarkerClickListener(marker) {
-// 			kakao.maps.event.addListener(marker, "click", function () {
-// 				console.log("marker.sub:", marker.sub);
-// 				document.getElementById("markerTitle").innerText = marker.getTitle();
-// 				document.getElementById("markerSub").innerText = marker.getSub();
-// 				modal.style.display = "inline-block";
-// 			});
-// 		}
-// 	},
-// });
