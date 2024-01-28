3a15d87ad30c2c16e119f45d5db7ecb5da2ba8ab
minchonang <green8085@naver.com>
2024-01-29 01:15:27 +0900

    css 변경

--- src/main/java/com/example/radio/Repository/DustRepositoy.java
+++ src/main/java/com/example/radio/Repository/DustRepositoy.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface DustRepositoy extends JpaRepository<Dust, Integer> {
+public interface DustRepositoy extends JpaRepository<Dust, String> {
 
 
 
